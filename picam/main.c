c4f2841afd9d376c62f164111f855e406eaf1323
Praveen M Nair <praveen.nair@icloud.com>
2016-03-17 21:19:00 -0700

    Initial Commit

---
+++ picam/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  picam
+//
+//  Created by Praveen Nair on 3/17/16.
+//  Copyright © 2016 Praveen Nair. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
